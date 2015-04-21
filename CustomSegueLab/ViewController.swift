688006dc74fa5fe45f3255eaf85713ff2a0a7b94
Claes Jacobsson <claes.jacobsson@gmail.com>
2015-04-21 12:15:24 +0200

    Initial Commit

---
+++ CustomSegueLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSegueLab
+//
+//  Created by Claes Jacobsson on 2015-04-21.
+//  Copyright (c) 2015 Claes Jacobsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
